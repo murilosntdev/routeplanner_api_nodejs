49cba25985297610373a4005e4310e8d44e29d57
murilosntdev <murilosntdev@gmail.com>
2023-06-21 02:16:29 -0300

    rota nao encontrada

--- src/routes.js
+++ src/routes.js
@@ -10,4 +10,22 @@ router.get('/', (req, res) => {
     })
 });
 
+router.use((req, res, next) => {
+    const error = new Error("Página Não Encontrada");
+    error.statusCode = 404;
+    next(error);
+});
+
+router.use((error, req, res, next) => {
+    const statusCode = error.statusCode || 500;
+    const message = error.message || "Internal Server Error";
+    res.status(statusCode);
+    res.json({
+        erro: {
+            status: statusCode,
+            mensagem: message
+        }
+    });
+});
+
 export default router;
