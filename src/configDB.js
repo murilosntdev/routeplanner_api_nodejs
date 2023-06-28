b8d5a8b388c1ac002db25b7ba710080db2769371
murilosntdev <murilosntdev@gmail.com>
2023-06-27 21:11:22 -0300

    conexao ao banco

---
+++ src/configDB.js
@@ -0,0 +1,12 @@
+import pg from "pg";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export const pool = new pg.Pool({
+    user: process.env.POSTGRE_USER,
+    host: process.env.POSTGRE_HOST,
+    database: process.env.POSTGRE_DATABASE,
+    password: process.env.POSTGRE_PASSWORD,
+    port: process.env.POSTGRE_PORT
+});
